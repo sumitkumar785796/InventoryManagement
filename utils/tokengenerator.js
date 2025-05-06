60cb8cd433cf79597ada8d2e59f67dd6d0d0c3b5
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2025-05-06 23:13:05 +0530

    deleted

--- utils/tokengenerator.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken")
-const { secretKey } = require("../utils")
-const generateToken = (user) => {
-    return jwt.sign({ user }, secretKey, { expiresIn: '10h' });
-}
-module.exports = generateToken
